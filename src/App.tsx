a09a948c4ed8e413e3278a7654a25eec516d5fb4
CyberCaban <andrewgamer63@gmail.com>
2023-01-14 20:03:49 +0300

    react rework + api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import { useState } from 'react'
-import { Socket } from 'socket.io'
+import { useEffect, useState } from 'react'
+import { Socket } from 'socket.io-client'
+import io from 'socket.io-client'
 import './App.css'
 
-function App() {
-  const [CurrentElement, setCurrentElement] = useState('')
-  // Socket = io()
+const ws_url = 'http://localhost:3000'
+const socket = io(ws_url)
 
+function App() {
+  //constants
+  const [CurrentElement, setCurrentElement] = useState('')
+  const [CurrentRoom, setCurrentRoom] = useState('')
+  const [User_id, setUser_id] = useState('')
+
+  //socket.io
+  useEffect(()=>{
+    socket.on('connect',()=>{
+      setUser_id(socket.id)
+      console.log('your id:' + socket.id);
+    })
+
+    return ()=>{
+      socket.off('connect')
+    }
+  })
+
+
+  //functions
   function copyToClipboard(e:any) {
     navigator.clipboard.writeText(e.target.textContent)
     alert('Copied to clipboard')
@@ -15,32 +35,66 @@ function App() {
     setCurrentElement(e.target.textContent.toLowerCase())
   }
 
+  function joinRoom(e:any) {
+    e.preventDefault()
+    let roomID = e.target[0].value
+    socket.emit('join room', roomID)
+    setCurrentRoom(roomID)
+
+    e.target[0].value = ''
+  }
+
+  function sendMessage(e:any) {
+    e.preventDefault()
+    let messageText = e.target[0].value
+
+    //создание даты чч:мм:сс
+    let date = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+
+    //создание объекта сообщения
+    const msg = {
+      author:socket.id,
+      messageText: messageText,
+      date: date
+    }
+
+    //отправка сообщения на сервер
+    if (messageText) {
+      console.log(msg);
+      socket.emit('send message', messageText)
+      e.target[0].value = ''
+    }
+  }
+
+  //render
   return (
     <div className="App">
       <div className="messenger">
-                <p>Join room</p>
-                <form id="joinRoom">
-                    <input id="roomID"></input>
-                </form>
-        
-                <p>Your ID: <span id="user_id" onClick={(e)=>copyToClipboard(e)}>fsfsefsefsef</span></p>
-                <p>Your room: <span id="current_room" onClick={(e)=>copyToClipboard(e)}></span></p>
-        
-                <ul id="messages"></ul>
-                <form id="message_form"><input id="message_input"></input></form>
-            </div>
+        <p>Join room</p>
+        <form id="joinRoom" onSubmit={(e)=>joinRoom(e)}>
+          <input id="roomID"></input>
+        </form>
+
+        <p>Your ID: <span id="user_id" onClick={(e)=>copyToClipboard(e)}>{User_id}</span></p>
+        <p>Your room: <span id="current_room" onClick={(e)=>copyToClipboard(e)}>{CurrentRoom}</span></p>
+
+        <ul id="messages"></ul>
+        <form id="message_form" onSubmit={(e)=>sendMessage(e)}>
+          <input id="message_input"></input>
+        </form>
+      </div>
     
-            <div className="field">
-                <button id="fight_button">FIGHT!</button>
-                <div id="current_img">
-                    <img src={`../src/assets/${CurrentElement}.png`} alt="" />
-                </div>
-                <div className="choose">
-                    <button onClick={(e)=>chooseElement(e)} id="rock_button">ROCK</button>
-                    <button onClick={(e)=>chooseElement(e)} id="paper_button">PAPER</button>
-                    <button onClick={(e)=>chooseElement(e)} id="scissors_button">SCISSORS</button>
-                </div>
-            </div>
+      <div className="field">
+        <button id="fight_button">FIGHT!</button>
+        <div id="current_img">
+          <img src={`../src/assets/${CurrentElement}.svg`} alt="" />
+        </div>
+        <div className="choose">
+          <button onClick={(e)=>chooseElement(e)} id="rock_button">ROCK</button>
+          <button onClick={(e)=>chooseElement(e)} id="paper_button">PAPER</button>
+          <button onClick={(e)=>chooseElement(e)} id="scissors_button">SCISSORS</button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import  App  from './App.js'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
-)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(<App />);
